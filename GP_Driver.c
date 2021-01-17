1e2c2688d84bce031c92ce7c6ca7edd3cbaa1770
ahmedmohmedels <ae@users.noreply.github.com>
2021-01-17 22:37:00 +0200

    Function Explained By Comment

--- GP_Driver.c
+++ GP_Driver.c
@@ -5,14 +5,14 @@ void GPIO_INIT(unsigned long port, unsigned long pin, unsigned long dir, unsigne
 	if(port == 0){//port A
 		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;//enable clock for port A
 		if(pin < 8){
-			GPIOA_CRL &= ~(0x0F<<(pin * 4));
-			GPIOA_CRL |= ((conf << 2UL) + dir) << (pin * 4);
+			GPIOA_CRL &= ~(0x0F << (pin * 4));//pin out field = 4 bits in this register //shift by number of pin and reset all field
+			GPIOA_CRL |= ((conf << 2UL) + dir) << (pin * 4);//pin out field = 4 bits in this register //shift by number of pin and setup all field
 		}
 		else{
-			RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;
+			RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;//enable clock for port A
 			if(pin > 7){
-				GPIOA_CRH &= ~(0x0F<<((pin-8) * 4));
-				GPIOA_CRH |= ((conf << 2UL) + dir) << ((pin-8) * 4);
+				GPIOA_CRH &= ~(0x0F << ((pin-8) * 4));//pin out field = 4 bits in this register //shift by number of pin-8 and reset all field
+				GPIOA_CRH |= ((conf << 2UL) + dir) << ((pin-8) * 4);//pin out field = 4 bits in this register //shift by number of pin-8 and setup all field
 			}
 		}
 	}
@@ -20,13 +20,13 @@ void GPIO_INIT(unsigned long port, unsigned long pin, unsigned long dir, unsigne
 	else if(port == 1){//port B
 		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;//enable clock for port B
 		if(pin < 8){
-			GPIOB_CRL &= ~(0x0F<<(pin * 4));
+			GPIOB_CRL &= ~(0x0F << (pin * 4));
 			GPIOB_CRL |= ((conf << 2UL) + dir) << (pin * 4);
 		}
 		else{
-			RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;
+			RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;//enable clock for port B
 			if(pin > 7){
-				GPIOB_CRH &= ~(0x0F<<((pin-8) * 4));
+				GPIOB_CRH &= ~(0x0F << ((pin-8) * 4));
 				GPIOB_CRH |= ((conf << 2UL) + dir) << ((pin-8) * 4);
 			}
 		}
@@ -35,13 +35,13 @@ void GPIO_INIT(unsigned long port, unsigned long pin, unsigned long dir, unsigne
 	else if(port == 2){//port C
 		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;//enable clock for port C
 		if(pin < 8){//for control low
-			GPIOC_CRL &= ~(0x0F<<(pin * 4));
+			GPIOC_CRL &= ~(0x0F << (pin * 4));
 			GPIOC_CRL |= ((conf << 2UL) + dir) << (pin * 4);
 		}
 		else{
-			RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;
+			RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;//enable clock for port C
 			if(pin > 7){//for control high
-				GPIOC_CRH &= ~(0x0F<<((pin-8) * 4));
+				GPIOC_CRH &= ~(0x0F << ((pin-8) * 4));
 				GPIOC_CRH |= ((conf << 2UL) + dir) << ((pin-8) * 4);
 			}
 		}
@@ -50,17 +50,17 @@ void GPIO_INIT(unsigned long port, unsigned long pin, unsigned long dir, unsigne
 
 unsigned long R_PIN(unsigned long port, unsigned long pin){
 	if(port == 0){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;//enable clock for port A
 			return ( GPIOA_IDR & ( 0x01 << pin ) );
 	}
 	//////////////////////////////
 	else if(port == 1){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;//enable clock for port B
 			return ( GPIOB_IDR & ( 0x01 << pin ) );
 	}
 	////////////////////////////
 	else if(port == 2){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;//enable clock for port C
 			return ( GPIOC_IDR & ( 0x01 << pin ) );
 	}
 	return 0;
@@ -69,7 +69,7 @@ unsigned long R_PIN(unsigned long port, unsigned long pin){
 
 void W_PIN(unsigned long port, unsigned long pin, unsigned long value){
 	if(port == 0){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;//enable clock for port A
 		if(value){
 			GPIOA_ODR |= value << pin;
 		}
@@ -79,7 +79,7 @@ void W_PIN(unsigned long port, unsigned long pin, unsigned long value){
 	}
 	////////////////////////////////////
 	else if(port == 1){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;//enable clock for port B
 		if(value){
 			GPIOB_ODR |= value << pin;
 		}
@@ -89,7 +89,7 @@ void W_PIN(unsigned long port, unsigned long pin, unsigned long value){
 	}
 	////////////////////////////////////////
 	else if(port == 2){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;//enable clock for port C
 		if(value){
 			GPIOC_ODR |= value << pin;
 		}
@@ -101,7 +101,7 @@ void W_PIN(unsigned long port, unsigned long pin, unsigned long value){
 
 void T_PIN(unsigned long port, unsigned long pin){
 	if(port == 0){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOA_KEY;//enable clock for port A
 		if(R_PIN(port, pin)){
 			W_PIN(port,pin,0);
 		}
@@ -111,7 +111,7 @@ void T_PIN(unsigned long port, unsigned long pin){
 	}
 	////////////////////////////////
 	else if(port == 1){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;
+		RCC_APB2ENR |= RCC_APB2EN_GPIOB_KEY;//enable clock for port B
 		if(R_PIN(port, pin)){
 			W_PIN(port,pin,0);
 		}
@@ -121,12 +121,12 @@ void T_PIN(unsigned long port, unsigned long pin){
 	}
 	///////////////////////////////
 	else if(port == 2){
-		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;
-		if(R_PIN(port, pin)){
-			W_PIN(port,pin,0);
+		RCC_APB2ENR |= RCC_APB2EN_GPIOC_KEY;//enable clock for port C
+		if(R_PIN(port, pin)){//check if pin high
+			W_PIN(port,pin,0);//make it low
 		}
-		else{
-			W_PIN(port,pin,1);
+		else{//else if low
+			W_PIN(port,pin,1);//make it high
 		}
 	}
 }
